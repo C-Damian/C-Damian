6a04b739c470b732466eac03cfc723dd37302cd2
C-Damian <138392709+C-Damian@users.noreply.github.com>
2025-05-17 19:38:11 -0500

    Worked on AboutSection and HeroSection

--- portfolio/src/components/HeroSection.jsx
+++ portfolio/src/components/HeroSection.jsx
@@ -1,3 +1,4 @@
+import { ArrowDown } from "lucide-react"
 import { Navbar } from "./Navbar"
 
 
@@ -17,7 +18,22 @@ export const HeroSection = () => {
               </span>
             <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">{" "}Trujillo</span>
           </h1>
+
+          <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">IT Professional | Product Delivery Lead | Cloud Technologies, Network Security | AWS, Python, ITIL, CompTIA A+, Network+, and Security+ Certified
+          </p>
+
+          <div className=" pt-4 opacity-0 animate-fade-in-delay-4">
+            <a href="#projects" className="cosmic-button">
+              View My Work
+            </a>
+          </div>
         </div>
+       </div>
+
+
+       <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
+       <span className="">Scroll</span>
+       <ArrowDown className="h-5 w-5 text-primary mt-1.5" />
        </div>
     </section>
   )
